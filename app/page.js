650fd82cc528235b5bdfd939326908411a274312
carlosmello122923-arch <carlosmello122923@gmail.com>
2026-01-06 13:03:35 -0300

    Create page.js
    Teste 3

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      background: 'black',
+      color: 'white',
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '24px'
+    }}>
+      CLAS Filmes — Site em teste 🎬
+    </div>
+  );
+}
